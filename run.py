3a4567f34fc7b416c20c22ca8bd44b1be46c0836
AlexVT100 <alex.vt100@protonmail.ch>
2024-11-26 10:24:42 +0300

    Simplify the command line syntax for the test mode

--- run.py
+++ run.py
@@ -16,7 +16,8 @@ from read_pressure.gauge_reader import GaugeReader
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('image_list',
-                        help='The list of images to be read',
+                        help='The list of images to be read in the reading mode (without --test-mode) or '
+                             'the list of test image patterns (with --test-mode).',
                         nargs='?', action='append')
     parser.add_argument('--save-debug-image', '-sdi',
                         help='Save the original image with debug drawings over it',
@@ -26,21 +27,12 @@ if __name__ == '__main__':
                              'Default is the directory of the current image',
                         nargs=1, dest='save_dir')
     parser.add_argument('--csv-file', dest='csv_file')
-    test_grp = parser.add_argument_group(
-        'Test mode',
-        'All options in this group must be used together. '
-        'A file name is constructed as "<DIR>/<PREFIX>P.PP[-V]<SUFFIX>", '
-        'where P.PP is an expected pressure value and V -- a decimal '
-        'number (optional).')
-    test_grp.add_argument('--test-image-dir', '-td',
-                          help='Read test images from the specified directory',
-                          metavar='DIR', dest='test_image_dir')
-    test_grp.add_argument('--test-image-prefix', '-tp',
-                          help='A prefix of the image file name, e. g. "gauge-"',
-                          metavar='PREFIX', dest='test_image_prefix')
-    test_grp.add_argument('--test-image-suffix', '-ts',
-                          help='A suffix of the image file name, e. g. ".png"',
-                          metavar='SUFFIX', dest='test_image_suffix')
+    parser.add_argument('--test-mode', '-t',
+                         help='The test mode. Each non-option argument is treated as a file name pattern.'
+                              'It may start with a directory and must contain the expected value macro "{V}" '
+                              'that expands to "P.PP[-R]", where P.PP is an expected pressure value and '
+                              'R is a decimal revision number (optional).',
+                         action='store_true', dest='test_mode')
     parser.add_argument('--debug',
                         help='Enables debug output',
                         dest='debug', action='store_true')
@@ -55,29 +47,34 @@ if __name__ == '__main__':
     else:
         csv_fs = None
 
-    test_args = (opts.test_image_dir, opts.test_image_prefix, opts.test_image_suffix)
-    if any(test_args):
-        test_mode = True
-        test_results = {}
+    if opts.test_mode:
         log.info('Test mode')
-        if not all(test_args):
-            log.fatal('Missing argument(s). In the test mode, all --test-*/-t* options are mandatory')
-            sys.exit(2)
-
         images = []
-        pattern = re.compile(opts.test_image_prefix + r'(\d\.\d\d)(-\d)?' + opts.test_image_suffix)
-        for entry in os.listdir(opts.test_image_dir):
-            if pattern.match(entry):
-                images.append(os.path.join(opts.test_image_dir, entry))
+        test_results = {}
+        macro_ptrn = r'(\d\.\d\d)(-\d)?'
+        macro_re = re.compile(macro_ptrn)
+
+        for image in opts.image_list:
+            if '{V}' not in image:
+                log.error('Pattern "{image}" doesn\'t contain "{V}" macro. Each test image pattern must contain it.')
+                sys.exit(2)
+
+            _dir = os.path.dirname(image)
+            _file = os.path.basename(image)
+            _ptrn = re.compile(_file.replace('{V}', macro_ptrn))
+
+            for entry in os.listdir(_dir):
+                if _ptrn.match(entry):
+                    images.append(os.path.join(_dir, entry))
+
         images.sort()
 
     else:
-        test_mode = False
         log.info('Reading mode')
         images = opts.image_list
 
     for image in images:
-        reader = GaugeReader(log, image, no_im_show=test_mode)
+        reader = GaugeReader(log, image, no_im_show=opts.test_mode)
         value = reader.exec()
 
         if opts.save_debug_image:
@@ -88,8 +85,8 @@ if __name__ == '__main__':
         else:
             log.info(f'value: {value}')
 
-        if test_mode:
-            expected = pattern.search(image).group(1)
+        if opts.test_mode:
+            expected = macro_re.search(image).group(1)
             measured = value
             test_results[image] = (expected, measured)
             log.info('')
@@ -101,7 +98,7 @@ if __name__ == '__main__':
             ts = datetime.fromtimestamp(reader.img_stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
             csv_fs.write(f'{ts},{value}\n')
 
-    if test_mode:
+    if opts.test_mode:
         fmt = '{:35s} {:>8s} {:>8s}  {:>5s}'
         log.info('Test results')
         log.info('------------------------------------------------------------')
